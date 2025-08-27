221815546c6974f549584d884ac703ed4203ab03
Prasunet <162030269+z9xqv7mkt3wl8psa@users.noreply.github.com>
2025-08-27 14:20:25 +0530

    Update page.tsx
    Signed-off-by: Prasunet <162030269+z9xqv7mkt3wl8psa@users.noreply.github.com>

--- src/app/service/softwaredevelopment/page.tsx
+++ src/app/service/softwaredevelopment/page.tsx
@@ -23,7 +23,7 @@ export default function SoftwareDevelopment() {
           className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
         >
           <source
-            src="/servicesvideo/softwaredevelopmentvideo.mp4"
+            src="/servicesvideo/softwaredevlopmentvideo.mp4"
             type="video/mp4"
           />
           Your browser does not support the video tag.
